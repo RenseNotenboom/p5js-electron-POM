f485761d181d023aafabe1f3ae8c38e9894013d4
raginrenz <rense@trimotion.nl>
2022-11-25 01:27:47 +0100

    Using Electron to make a native JS app!

--- public/sketch.js
+++ public/sketch.js
@@ -1,33 +1,93 @@
-const easerCount = 500
-const easing = 0.05
-const diameter = 10
-let easer = []
+let knipperMode, engineMode, lightMode;
 
+
+
+function preload(){
+  //loading all sounds here
+  START_START = loadSound('Sounds/START_01_START.wav');
+  START_LOOP = loadSound('Sounds/START_01_LOOP.wav');
+  START_STOP = loadSound('Sounds/START_01_STOP.wav');
+  //START_ENGINE =
+}
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  // console.log(`${windowWidth}, ${windowHeight}`)
+
+  console.log("POM V.01");
+  createCanvas(windowWidth, windowHeight);
+  //setting up OSC
+  setupOsc(1200, 3334);
   noStroke()
   background(255)
-
-  for (let i = 0; i < easerCount; i++) {
-    let e = new Easer(width / 2, height / 2, diameter, easing)
-    easer.push(e)
-  }
 }
 
 function draw() {
-  background(255)
+  onEngine();
+	background(0, 0, 255);
+	fill(0, 255, 0);
+	fill(0);
 
-  for (let i = 0; i < easer.length; i++) {
-    easer[i].update()
-    easer[i].render()
+}
+//what to do when someone toggles the engine switch on.
+function onEngine(){
+  if (engineMode == 1 && START_01_START.isPlaying() == false){
+    START_01_START.play();
+    engineMode =2;
   }
+ if(engineMode ==2 && START_01_START.isPlaying() == false && START_01_LOOP.isPlaying() == false ){
+ START_LOOP.play();
+
+}
+if(engineMode == 4 ){
+  console.log("PRESSED STOP");
+  START_STOP.play();
+  START_LOOP.stop();
+  engineMode=0;
+}
+}
+//listening to all values and assigning them here
+function receiveOsc(address, value) {
+	console.log("received OSC: " + address + ", " + value);
+
+	if (address == '/knipperMode') {
+    
+		knipperMode = value;
+	}
+  if (address == '/lightMode') {
+    
+		lightMode = value;
+	}
+  if (address == '/engineMode') {
+		engineMode = value;
+	}
 }
 
+//OSC STUFF
+function sendOsc(address, value) {
+	socket.emit('message', [address].concat(value));
+}
+//CHANGE IP AT OSCPORTIN/OUT
+function setupOsc(oscPortIn, oscPortOut) {
+	var socket = io.connect('http://127.0.0.1:8081', { port: 8081, rememberTransport: false });
+	socket.on('connect', function() {
+		socket.emit('config', {
+   
+			server: { port: oscPortIn,  host: '192.168.1.151'},
+			client: { port: oscPortOut, host: '192.168.1.151'}
+		});
+	});
+	socket.on('message', function(msg) {
+		if (msg[0] == '#bundle') {
+			for (var i=2; i<msg.length; i++) {
+				receiveOsc(msg[i][0], msg[i].splice(1));
+			}
+		} else {
+			receiveOsc(msg[0], msg.splice(1));
+		}
+	});
+}
+
+
 function mousePressed() {
-  for (let i = 0; i < easer.length; i++) {
-    easer[i].setTarget(mouseX, mouseY)
-  }
+
 }
 
 function windowResized() {
@@ -35,39 +95,6 @@ function windowResized() {
 }
 
 
-function Easer(xpos, ypos, diameter, newEasing) {
-  this.x = xpos
-  this.y = ypos
-  this.targetX = this.x
-  this.targetY = this.y
-  this.d = diameter
-  this.ease = newEasing
-  this.clr = color(random(0, 255), random(0, 255), random(0, 255), 127)
 
-  this.render = function() {
-    fill(this.clr)
-    ellipse(this.x, this.y, this.d, this.d)
-  }
 
-  this.update = function() {
-    let dx = this.targetX - this.x
-    let dy = this.targetY - this.y
-    if (abs(dx) > 0.1 || abs(dy) > 0.1) {
-      this.x += dx * this.ease
-      this.y += dy * this.ease
-    } else {
-      this.setRandomTarget()
-    }
-  }
-
-  this.setTarget = function(xpos, ypos) {
-    this.targetX = xpos
-    this.targetY = ypos
-  }
-
-  this.setRandomTarget = function() {
-    this.targetX = random(0, width)
-    this.targetY = random(0, height)
-  }
-}
 
